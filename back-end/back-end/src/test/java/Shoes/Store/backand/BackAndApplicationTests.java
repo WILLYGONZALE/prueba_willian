4229042472674fa4f440097243d25fef837f4e97
WILLYGONZALE <willianstebangonzalezcortes@gmail.com>
2024-05-09 17:33:38 -0500

    se agrego el mockup de productos

---
+++ back-end/back-end/src/test/java/Shoes/Store/backand/BackAndApplicationTests.java
@@ -0,0 +1,13 @@
+package Shoes.Store.backand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
